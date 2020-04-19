06bb875326b72e18b1b33fcd6c69290c5ae25fec
maze@30022 <maze1234556@sjtu.edu.cn>
2020-04-20 06:37:07 +0800

    "exp1 first try to get circle traj"

---
+++ part_circle_exp1.py
@@ -0,0 +1,44 @@
+import gym
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines import PPO2
+from stable_baselines import PPO1
+from particle_env_continuous import PrticleEnv
+import os
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # env = gym.make('CartPole-v1')
+    env = DummyVecEnv([lambda: PrticleEnv(alpha=1,beta=10,win_thre=1, max_timestep=256,for_circle_traj=True)])
+    # Optional: PPO2 requires a vectorized environment to run
+    # the env is now wrapped automatically when passing it to the constructor
+    # env = DummyVecEnv([lambda: env])
+    # env = VecNormalize(env, norm_obs=True, norm_reward=False,
+    #                 clip_obs=10.)
+
+
+    save_name = "model/part_circle_exp1"
+    if not os.path.exists(save_name):
+        os.makedirs(save_name)
+    # model = PPO2(MlpPolicy, env, verbose=0,tensorboard_log="./ppo2_particle_tensorboard/",n_cpu_tf_sess=1)
+    model = PPO1(MlpPolicy,env,verbose=0,\
+            timesteps_per_actorbatch=256,
+            tensorboard_log=save_name,\
+            policy_kwargs={"net_arch": [dict(vf=[64,64,64], pi=[64,64,64])]},\
+            optim_stepsize = 3e-4,
+            optim_batchsize=256,
+            optim_epochs = 4,
+            schedule='linear',
+            n_cpu_tf_sess=16)
+    model.learn(total_timesteps=int(2e6))
+    model.save(save_name)
+
+    del model # remove to demonstrate saving and loading
--- particle_env_continuous.py
+++ particle_env_continuous.py
@@ -10,7 +10,7 @@ class PrticleEnv(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, alpha=10, beta=1, win_thre=1, max_timestep=-1):
+    def __init__(self, alpha=10, beta=1, win_thre=1, max_timestep=-1,for_circle_traj = False):
         super(PrticleEnv, self).__init__()
         # Define action and observation space
         # They must be gym.spaces objects
@@ -42,7 +42,8 @@ class PrticleEnv(gym.Env):
         self.viewer = None
         self.curr_timestep = 0
         self.max_timestep = max_timestep
-
+        self.init_dis = 0
+        self.for_circle_traj = for_circle_traj
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
@@ -68,6 +69,9 @@ class PrticleEnv(gym.Env):
         self.state = np.array([x,x_dot,y,y_dot])
         self.curr_timestep += 1
 
+
+
+
         done = False
         if  x < -self.X_lim \
                 or x > self.X_lim \
@@ -83,46 +87,39 @@ class PrticleEnv(gym.Env):
         if self.max_timestep > 0 and self.curr_timestep == self.max_timestep:
             reward = -1*self.beta
             done = True
+
+        if self.for_circle_traj:
+            reward = self.get_reward(x,y,x_dot,y_dot)
+
+
         return self.state, reward, done, {}
+
+    def get_reward(self,x,y,x_dot,y_dot):
+        # reward for keep the same dist from the goal
+        distance = math.sqrt((x-self.x_goal)**2+(y-self.y_goal)**2)
+        shift_from_init_dis = math.sqrt((distance - self.init_dis)**2)
+        shift_reward = 10.*math.exp(-shift_from_init_dis)
+
+        # reward for higher speed
+        velocity = math.sqrt(x_dot**2+y_dot**2)
+        velocity_reward = min(math.exp(velocity)-1,10)
+
+        total_reward = shift_reward * velocity_reward
+
+        return total_reward
+
+
 
     def reset(self):
         # Reset the state of the environment to an initial state
         self.state = np.zeros(4,dtype=np.float32)
         self.state[0] = np.random.uniform(-self.X_lim,self.X_lim)
         self.state[2] = np.random.uniform(-self.Y_lim,self.Y_lim)
+        x, x_dot, y, y_dot= self.state
+        self.init_dis = math.sqrt((x-self.x_goal)**2+(y-self.y_goal)**2)
         self.curr_timestep = 0
         return self.state  
-    
-    # def render(self, mode='human', close=False):
-    #     # Render the environment to the screen
-    #     state = self.state
-    #     x, x_dot, y, y_dot= state
-    #     distance = math.sqrt((x-self.x_goal)**2+(y-self.y_goal)**2)
-    #     self.print_particle_map(x,y)
-    #     print("dist:%.2f x:%.2f y:%.2f x_dot:%.2f y_dot:%.2f " %(distance,x,y,x_dot,y_dot))
 
-
-
-    
-    # def print_particle_map(self, x,y):
-    #     grid = [['.' for _ in range(int(2*self.Y_lim))] for _ in range(int(2*self.X_lim))]
-    #     grid[int(x+self.X_lim)][int(y+self.Y_lim)] = 'o'
-
-    #     c=grid
-    #     #print(c)
-    #     gridLen=len(grid)
-    #     cyctime=len(grid[0])
-    #     #print(cyctime) 
-    #     i=0
-    #     j=0
-    #     for j in range(cyctime):
-    #         if j < cyctime :
-    #             for i in range(gridLen):
-    #                 if i < gridLen :
-    #                     print(c[i][j],end='')
-    #                     i=i+1
-    #         print()
-    #         j=j+1
 
     def render(self, mode='human', close=False):
         screen_width = 600
@@ -138,53 +135,34 @@ class PrticleEnv(gym.Env):
             self.viewer = rendering.Viewer(screen_width, screen_height)
             # add goal
             goal = rendering.make_circle(20,30)
-            #添加台车转换矩阵属性
+            #Add transform attribute to the cart
             self.goaltrans = rendering.Transform()
             goal.add_attr(self.goaltrans)
             goalx = screen_width/2.0 # MIDDLE OF CART
             goaly = screen_height/2.0 # MIDDLE OF CART
-            #设置平移属性
+            #set the translation attribute of cart
             self.goaltrans.set_translation(goalx, goaly)
             self.viewer.add_geom(goal)
 
-            # 创建台车
+            # creat cart
             l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
             # axleoffset =cartheight/4.0
             cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
             cart.set_color(.8,.6,.4)
 
-            #添加台车转换矩阵属性
+            #Add transform attribute to the cart
             self.carttrans = rendering.Transform()
             cart.add_attr(self.carttrans)
-            #加入几何体台车
+            #Add Geometric cart
             self.viewer.add_geom(cart)
-            #创建摆杆
-            # l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-            # pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            # pole.set_color(.8,.6,.4)
-            #添加摆杆转换矩阵属性
-            # self.poletrans = rendering.Transform(translation=(0, axleoffset))
-            # pole.add_attr(self.poletrans)
-            # pole.add_attr(self.carttrans)
-            #加入几何体
-            # self.viewer.add_geom(pole)
-            #创建摆杆和台车之间的连接
-            # self.axle = rendering.make_circle(polewidth/2)
-            # self.axle.add_attr(self.poletrans)
-            # self.axle.add_attr(self.carttrans)
-            # self.axle.set_color(.5,.5,.8)
-            # self.viewer.add_geom(self.axle)
-            #创建台车来回滑动的轨道，即一条直线
-            # self.track = rendering.Line((0,carty), (screen_width,carty))
-            # self.track.set_color(0,0,0)
-            # self.viewer.add_geom(self.track)
+
 
         if self.state is None: return None
 
         x,x_dot,y,y_dot = self.state
         cartx = x*scale+screen_width/2.0 # MIDDLE OF CART
         carty = y*scale+screen_height/2.0 # MIDDLE OF CART
-        #设置平移属性
+            #set the translation attribute of cart
         self.carttrans.set_translation(cartx, carty)
         # self.poletrans.set_rotation(-x[2])
 
